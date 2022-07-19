ca096d9da80769bf336f06f4a6abc0cbebaeae7f
schoobani <saeed.choobani@gmail.com>
2022-07-19 10:48:27 +0000

    adding setup.py file

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="mingpt", # Replace with your own username
+    version="0.0.1",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aitechnologies-it/minGPT",
+    project_urls={
+        "Bug Tracker": "https://github.com/aitechnologies-it/minGPT/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "mingpt"},
+    packages=setuptools.find_packages(where="mingpt"),
+    python_requires=">=3.7",
+    install_requires=required,
+)
